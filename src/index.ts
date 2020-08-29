8d1641a5ced514090181de02fa40e57c3be1c90d
Quin <quin@cheesits456.dev>
2020-08-28 22:26:36 -0700

    move ./index.js to ./src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 const { APIMessage, TextChannel } = require("discord.js");
 
 module.exports = () => {
-
 	// Replace the "send" function for Guild Text Channels
 	TextChannel.prototype.send = async function (content, options) {
-
 		// New added code
 		if (typeof content === "string")
 			content = content.replace(/<@&(\d+)>/g, (match, id) =>
@@ -14,10 +12,10 @@ module.exports = () => {
 				this.guild.me.hasPermission(131072) &&
 				// if the role is not normally mentionable, or if the role doesn't exist in the guild
 				!this.guild.roles.cache.get(id)?.mentionable
-					// replace the mention with the role's name if it's available
-					? `\`@${this.guild.roles.cache.get(id)?.name || "unknown-role"}\``
-					// else pass through the original mention
-					: match
+					? // replace the mention with the role's name if it's available
+					  `\`@${this.guild.roles.cache.get(id)?.name || "unknown-role"}\``
+					: // else pass through the original mention
+					  match,
 			);
 
 		// Original code for the "send" function
@@ -25,14 +23,9 @@ module.exports = () => {
 		if (content instanceof APIMessage) apiMessage = content.resolveData();
 		else {
 			apiMessage = APIMessage.create(this, content, options).resolveData();
-			if (Array.isArray(apiMessage.data.content))
-				return Promise.all(apiMessage.split().map(this.send.bind(this)));
+			if (Array.isArray(apiMessage.data.content)) return Promise.all(apiMessage.split().map(this.send.bind(this)));
 		}
 		const { data, files } = await apiMessage.resolveFiles();
-		return this.client.api.channels[this.id].messages
-			.post({ data, files })
-			.then(d => this.client.actions.MessageCreate.handle(d).message);
-
+		return this.client.api.channels[this.id].messages.post({ data, files }).then(d => this.client.actions.MessageCreate.handle(d).message);
 	};
-	
 };
